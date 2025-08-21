7a1daa670a30395010cc92b63266e5deb60bbe38
memoyil <2213635+memoyil@users.noreply.github.com>
2025-08-21 10:08:58 +0200

    chore: Typesafeurl

--- apps/web/_next-typesafe-url_.d.ts
+++ apps/web/_next-typesafe-url_.d.ts
@@ -69,9 +69,6 @@ declare module "@@@next-typesafe-url" {
     "/info/v3/tokens": StaticRoute;
     "/invite/[code]": StaticRoute;
     "/limit-orders": StaticRoute;
-    "/liquid-staking/[contract]": StaticRoute;
-    "/liquid-staking": StaticRoute;
-    "/liquid-staking/request-withdraw/[contract]": StaticRoute;
     "/liquidity/[tokenId]": StaticRoute;
     "/liquidity/create": StaticRoute;
     "/liquidity": StaticRoute;
@@ -99,7 +96,6 @@ declare module "@@@next-typesafe-url" {
     "/profile": StaticRoute;
     "/profile/pancake-collectibles": StaticRoute;
     "/remove/[tokenId]": StaticRoute;
-    "/simple-staking": StaticRoute;
     "/sol/wallet-test": StaticRoute;
     "/stable/[address]": StaticRoute;
     "/stable/add/[[...currency]]": StaticRoute;
